d04ba98680186b7796cf0a00b43679d91f33bf84
AzureAD\IharAntonenka <i_antonenka@locport.io>
2022-10-14 21:00:13 +0300

    update: implement stack

--- src/stack.js
+++ src/stack.js
@@ -13,21 +13,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor() {
+  this.array = [];
+ }
+
+  push(element) {
+    this.array.push(element)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.array.length === 0) {
+      return undefined;
+    }
+    return this.array.splice(-1)[0];
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.array.length === 0) {
+      return undefined;
+    }
+    return this.array[this.array.length-1]
   }
 }
+
+// const stack = new Stack();
+//     stack.push(5);
+//     stack.push(6);
+//     stack.push(7);
+//     let dfd = stack.peek();//, 7);
+//     dfd = stack.pop()//, 7);
+//     dfd = stack.peek()//, 6);
 
 module.exports = {
   Stack
